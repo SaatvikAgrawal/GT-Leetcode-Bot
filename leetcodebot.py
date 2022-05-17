3b5f417e3133cd090c437c41bb993ee07d552a0f
flextian <atian9@gatech.edu>
2022-05-17 02:25:21 -0400

    renamed variables for clarity, wrote method to call api, updated link to new api

--- leetcodebot.py
+++ leetcodebot.py
@@ -7,11 +7,13 @@ from discord.ext import commands
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-# This is added so that many files can reuse the function get_database()
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 PRODUCTION = os.getenv("PRODUCTION")
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+
+print(CONNECTION_STRING)
 
 bot = None
 if int(PRODUCTION) == 1:
@@ -20,24 +22,35 @@ if int(PRODUCTION) == 1:
 else:
     bot = commands.Bot(command_prefix="!")
 
-print(BOT_TOKEN)
-
 
 def get_database():
-    # Provide the MongoDB atlas url to connect Python to MongoDB using PyMongo
-    CONNECTION_STRING = os.getenv("CONNECTION_STRING")
-
-    # Create a connection using MongoClient.
+    # Create a connection using MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     # Create the database
     return client['users']
 
 
+def call_leetcode_api(username):
+    api_url = 'https://leetcode.com/graphql?query='
+    query = """
+        { matchedUser(username: """ + '"' + username + '"' + """) {
+        username
+        submitStats: submitStatsGlobal {
+        acSubmissionNum {
+        difficulty
+        count
+        submissions
+        }
+        }
+        }
+        }
+    """
+    return dict(requests.get(api_url + query).json())
+
+
 # Method to update all of the scores.
 def update(accounts_reference=None):
-    # file = open("accounts.csv", "r")
-
     # Key: leetcode username | Value: discord id
     accounts = {}
 
@@ -46,37 +59,23 @@ def update(accounts_reference=None):
     users_list = list(users_collection)
     for user in users_list:
         accounts[user["leetcode_username"]] = user["discord_id"]
-    print(users_list)
-    print(accounts)
 
     # Pass reference of accounts (a dictionary from leetcode username to discord user id) to create a copy
     if accounts_reference is not None:
         for accountKeys in accounts.keys():
             accounts_reference[accountKeys] = accounts[accountKeys]
 
-    accounts = accounts.keys()
+    account_names = accounts.keys()
+    # maps leetcode username to score
     score_totals = {}
-    for account in accounts:
-        apiUrl = 'https://leetcode.com/graphql?query='
-        query = """
-            { matchedUser(username: """ + '"' + account + '"' + """) {
-            username
-            submitStats: submitStatsGlobal {
-            acSubmissionNum {
-            difficulty
-            count
-            submissions
-            }
-            }
-            }
-            }
-        """
-        a = dict(requests.get(apiUrl + query).json())
-        user_data_parsed = a['data']['matchedUser']['submitStats']['acSubmissionNum']
+    for account in account_names:
+
+        response = call_leetcode_api(account)
+        user_data_parsed = response['data']['matchedUser']['submitStats']['acSubmissionNum']
         easy = user_data_parsed[1]['count']
         medium = user_data_parsed[2]['count']
         hard = user_data_parsed[3]['count']
-        # (easySolved * 1) + (mediumSolved * 3) + (hardSolved * 5)
+
         total = easy + (3 * medium) + (5 * hard)
         score_totals[account] = total
         print(score_totals)
@@ -86,40 +85,47 @@ def update(accounts_reference=None):
 
 @bot.command()
 async def top(ctx):
+    # Key: leetcode username | Value: discord id
     accounts = {}
+
     # Pass a reference to accounts
     score_totals = update(accounts)
 
-    count = 1  # Leaderboard position
+    position_no = 1
     leaderboard = ""
+
     # Sort data by the total score values, from greatest to least
     sorted_score_totals = sorted(score_totals.items(), reverse=True, key=operator.itemgetter(1))
-    for element in sorted_score_totals:
-        leaderboard += (str(count) + ". " + str(await bot.fetch_user(accounts[str(element[0])])) + ": " + str(
-            element[1]) + " " + "\n")
-        count += 1
+
+    for score in sorted_score_totals:
+        leaderboard += (str(position_no) + ". " + str(await bot.fetch_user(accounts[str(score[0])])) + ": " + str(
+            score[1]) + " " + "\n")
+        position_no += 1
     await ctx.send(leaderboard)
 
 
 @bot.command()
-async def link(ctx, new_account):
-    new_account = str(new_account)
+async def link(ctx, account_name):
+    # Name of leetcode account to add to the database
+    account_name = str(account_name)
     database = get_database()
-    # print("Current user in database: " + list(database["users"].find({"discord_id" : ctx.author.id})))
-    # If the user exists in the database, then they have already linked an account
+
+    # If the user already exists
     if list(database["users"].find({"discord_id": ctx.author.id})):
         await ctx.send("You already linked an account")
+
     # If the status is "error," then the account does not exist (or some other issue has occurred)
-    elif dict(requests.get("https://leetcode-stats-api.herokuapp.com/" + new_account).json())["status"] == "error":
+    elif "errors" in call_leetcode_api(account_name):
         await ctx.send("Error while linking account")
-    # If the status is not error, then the account has been successfully found in the database and is added to the file
+
+    # If account is valid and not in database, add to database
     else:
         new_user = {
-            "leetcode_username": new_account,
+            "leetcode_username": account_name,
             "discord_id": ctx.author.id
         }
         database["users"].insert_one(new_user)
-        await ctx.send("Set Leetcode account for " + str(ctx.author) + " to " + new_account)
+        await ctx.send(f"Set Leetcode account for {str(ctx.author)} to {account_name}")
 
 
 @bot.event
