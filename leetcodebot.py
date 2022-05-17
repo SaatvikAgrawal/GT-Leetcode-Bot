16733ae14c7595bf83c73df2951b55b8dce6ba7b
flextian <atian9@gatech.edu>
2022-05-17 00:36:14 -0400

    minor changes to variable names (python uses camel case), deleted try & except for api connection

--- leetcodebot.py
+++ leetcodebot.py
@@ -35,73 +35,66 @@ def get_database():
 
 
 # Method to update all of the scores.
-def update(accountsReference=None):
+def update(accounts_reference=None):
     # file = open("accounts.csv", "r")
 
     # Key: leetcode username | Value: discord id
     accounts = {}
 
     database = get_database()
-    usersCollection = database["users"].find()
-    usersList = list(usersCollection)
-    for user in usersList:
+    users_collection = database["users"].find()
+    users_list = list(users_collection)
+    for user in users_list:
         accounts[user["leetcode_username"]] = user["discord_id"]
-    print(usersList)
+    print(users_list)
     print(accounts)
 
     # Pass reference of accounts (a dictionary from leetcode username to discord user id) to create a copy
-    if accountsReference is not None:
+    if accounts_reference is not None:
         for accountKeys in accounts.keys():
-            accountsReference[accountKeys] = accounts[accountKeys]
+            accounts_reference[accountKeys] = accounts[accountKeys]
 
     accounts = accounts.keys()
-    scoreTotals = {}
+    score_totals = {}
     for account in accounts:
-        # Retrieve stats from Leetcode and convert to dictionary
-        try:
-            apiUrl = 'https://leetcode.com/graphql?query='
-            query = """
-                { matchedUser(username: """ + '"' + account + '"' + """) {
-                username
-                submitStats: submitStatsGlobal {
-                acSubmissionNum {
-                difficulty
-                count
-                submissions
-                }
-                }
-                }
-                }
-            """
-            a = dict(requests.get(apiUrl + query).json())
-            userDataParsed = a['data']['matchedUser']['submitStats']['acSubmissionNum']
-            easy = userDataParsed[1]['count']
-            medium = userDataParsed[2]['count']
-            hard = userDataParsed[3]['count']
-            # (easySolved * 1) + (mediumSolved * 3) + (hardSolved * 5)
-            total = easy + (3 * medium) + (5 * hard)
-            scoreTotals[account] = total
-            print(scoreTotals)
-        except:
-            stats = dict(requests.get("https://leetcode-stats-api.herokuapp.com/" + account).json())
-            # (easySolved * 1) + (mediumSolved * 3) + (hardSolved * 5)
-            total = stats["easySolved"] + (3 * stats["mediumSolved"]) + (5 * stats["hardSolved"])
-            scoreTotals[account] = total
+        apiUrl = 'https://leetcode.com/graphql?query='
+        query = """
+            { matchedUser(username: """ + '"' + account + '"' + """) {
+            username
+            submitStats: submitStatsGlobal {
+            acSubmissionNum {
+            difficulty
+            count
+            submissions
+            }
+            }
+            }
+            }
+        """
+        a = dict(requests.get(apiUrl + query).json())
+        user_data_parsed = a['data']['matchedUser']['submitStats']['acSubmissionNum']
+        easy = user_data_parsed[1]['count']
+        medium = user_data_parsed[2]['count']
+        hard = user_data_parsed[3]['count']
+        # (easySolved * 1) + (mediumSolved * 3) + (hardSolved * 5)
+        total = easy + (3 * medium) + (5 * hard)
+        score_totals[account] = total
+        print(score_totals)
 
-    return scoreTotals
+    return score_totals
 
 
 @bot.command()
 async def top(ctx):
     accounts = {}
     # Pass a reference to accounts
-    scoreTotals = update(accounts)
+    score_totals = update(accounts)
 
     count = 1  # Leaderboard position
     leaderboard = ""
     # Sort data by the total score values, from greatest to least
-    sortedScoreTotals = sorted(scoreTotals.items(), reverse=True, key=operator.itemgetter(1))
-    for element in sortedScoreTotals:
+    sorted_score_totals = sorted(score_totals.items(), reverse=True, key=operator.itemgetter(1))
+    for element in sorted_score_totals:
         leaderboard += (str(count) + ". " + str(await bot.fetch_user(accounts[str(element[0])])) + ": " + str(
             element[1]) + " " + "\n")
         count += 1
@@ -109,24 +102,24 @@ async def top(ctx):
 
 
 @bot.command()
-async def link(ctx, newAccount):
-    newAccount = str(newAccount)
+async def link(ctx, new_account):
+    new_account = str(new_account)
     database = get_database()
     # print("Current user in database: " + list(database["users"].find({"discord_id" : ctx.author.id})))
     # If the user exists in the database, then they have already linked an account
-    if list(database["users"].find({"discord_id": ctx.author.id})) != []:
+    if list(database["users"].find({"discord_id": ctx.author.id})):
         await ctx.send("You already linked an account")
     # If the status is "error," then the account does not exist (or some other issue has occurred)
-    elif dict(requests.get("https://leetcode-stats-api.herokuapp.com/" + newAccount).json())["status"] == "error":
+    elif dict(requests.get("https://leetcode-stats-api.herokuapp.com/" + new_account).json())["status"] == "error":
         await ctx.send("Error while linking account")
     # If the status is not error, then the account has been successfully found in the database and is added to the file
     else:
-        newUser = {
-            "leetcode_username": newAccount,
+        new_user = {
+            "leetcode_username": new_account,
             "discord_id": ctx.author.id
         }
-        database["users"].insert_one(newUser)
-        await ctx.send("Set Leetcode account for " + str(ctx.author) + " to " + newAccount)
+        database["users"].insert_one(new_user)
+        await ctx.send("Set Leetcode account for " + str(ctx.author) + " to " + new_account)
 
 
 @bot.event
